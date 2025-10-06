74e046f694698bbf5599f1aa993deab70f8454bb
falinpavel-macbook <aloverapf@yandex.ru>
2025-10-06 12:56:14 +0300

    fix test.yml

--- fixtures/settings.py
+++ fixtures/settings.py
@@ -1,10 +1,8 @@
 import pytest
-from dotenv import load_dotenv
 
 from config import Settings
 
 
 @pytest.fixture(scope="session")
 def settings() -> Settings:
-    load_dotenv()
     return Settings()
