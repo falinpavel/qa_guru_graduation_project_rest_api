3e28d06eb7ef790b3a68c3f40bba46bdfbdc3296
falinpavel-macbook <aloverapf@yandex.ru>
2025-10-06 12:48:43 +0300

    fix error

--- fixtures/settings.py
+++ fixtures/settings.py
@@ -1,8 +1,10 @@
 import pytest
+from dotenv import load_dotenv
 
 from config import Settings
 
 
 @pytest.fixture(scope="session")
 def settings() -> Settings:
+    load_dotenv()
     return Settings()
