4a2505a4b2c8b4f960d355f28e00123df6a5bfa5
falinpavel-macbook <aloverapf@yandex.ru>
2025-10-10 12:01:48 +0300

    added alias for schemas

--- schema/operations.py
+++ schema/operations.py
@@ -6,20 +6,20 @@ from tools.fakers import fake
 class CreateOperationSchema(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    debit: float | None = Field(default_factory=fake.money)
-    credit: float | None = Field(default_factory=fake.money)
-    category: str = Field(default_factory=fake.category)
-    description: str = Field(default_factory=fake.sentence)
+    debit: float | None = Field(alias="Debit", default_factory=fake.money)
+    credit: float | None = Field(alias="Credit", default_factory=fake.money)
+    category: str = Field(alias="Category", default_factory=fake.category)
+    description: str = Field(alias="Description", default_factory=fake.sentence)
     transaction_date: date = Field(alias="transactionDate", default_factory=fake.date)
 
 
 class UpdateOperationSchema(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    debit: float | None = Field(default_factory=fake.money)
-    credit: float | None = Field(default_factory=fake.money)
-    category: str | None = Field(default_factory=fake.category)
-    description: str | None = Field(default_factory=fake.sentence)
+    debit: float | None = Field(alias="Debit", default_factory=fake.money)
+    credit: float | None = Field(alias="Credit", default_factory=fake.money)
+    category: str | None = Field(alias="Category", default_factory=fake.category)
+    description: str | None = Field(alias="Description", default_factory=fake.sentence)
     transaction_date: date | None = Field(alias="transactionDate", default_factory=fake.date)
 
 
